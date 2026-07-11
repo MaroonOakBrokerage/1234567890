23e1eb0b824d0b69696cfa3bc802d34af15bb3a2
MaroonOakBrokerage <cole.t.hindes@outlook.com>
2026-07-11 18:49:28 -0500

    updated

--- maroon-oak-brokerage 2/next.config.ts
+++ maroon-oak-brokerage 2/next.config.ts
@@ -2,14 +2,32 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
   images: {
     formats: ["image/avif", "image/webp"],
+
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
     ],
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: false,
   },
 };
 
